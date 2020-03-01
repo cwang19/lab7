5e61859b592fb557d4001b420d1017dd8f5e36ce
cwang19 <cwang265@gmail.com>
2020-03-01 12:34:29 -0800

    syntax fix

--- lab7_part3.ml
+++ lab7_part3.ml
@@ -123,7 +123,7 @@ with the elements inverted, *without using any of the `IntListStack`
 methods*.
 ......................................................................*)
 
-let invert_stack (s : IntListStack.stack) : IntListStack.stack =
+let invert_stack : IntListStack.stack -> IntListStack.stack =
   List.rev ;;
 
 (* Now what would be the result of the top operation on a stack
